d6b12bda024bb6725d72260dc398ddf9fbfb2465
marloncori <84450525+marloncori@users.noreply.github.com>
2021-10-12 09:09:19 +0200

    Update blink.sh

--- blink.sh
+++ blink.sh
@@ -3,6 +3,7 @@
 #let us say I have a LED connected to RPi GPIO17
 GPIO="$1"
 # variable that catches the entered arg[0] from the command line
+# when running this script I have to write the pin number which will be toggled every second.
 
 SYS=/sys/class/gpio
 DEV=/sys/class/gpio/gpio$GPIO
