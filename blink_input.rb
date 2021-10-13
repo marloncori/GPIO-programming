d30c6b0f00ab37ed23217fb9595e0e61a716b8b9
marloncori <84450525+marloncori@users.noreply.github.com>
2021-10-13 22:43:03 +0200

    Create blink_input.rb

---
+++ blink_input.rb
@@ -0,0 +1,33 @@
+require "pigpio"
+include Pigpio::Constant
+
+pi=Pigpio.new
+unless pi.connect
+  exit -1
+end
+
+led = pi.gpio(18)
+sensor = pi.gpio(22)
+
+led.mode = PI_OUTPUT
+led.pud = PI_PUD_OFF
+
+sensor.mode = PI_INPUT
+sensor.pud = PI_PUD_OFF
+
+trap("SIGINT") { throw :ctrl_c }
+
+ catch :ctrl_c do
+  begin
+    if sensor.read == 1
+         led.write 0
+         puts "No object detected."
+    else
+         led.write 1
+         puts "Object has been detected!!!"
+    end
+  rescue Exception
+     puts "Not printed"
+     pi.stop
+   end
+ end
